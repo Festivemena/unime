760606f6e50543556b43147d4d1ffa19861f5f18
Efemena <88979259+Festivemena@users.noreply.github.com>
2024-06-14 14:24:08 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
       <body className={poppins.className}>{children}</body>
     </html>
   );
